7bd58d4a8465b589a2bf153fe4cfcdb453475b48
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-19 23:10:34 +0530

    initial commit

--- smartcontract/RandomNumbers.sol
+++ smartcontract/RandomNumbers.sol
@@ -9,6 +9,7 @@ contract RandomNumbers {
         uint256 playerid;
         uint256 totalwins;
         uint256 totallosts;
+        uint256 betamount;
         bool verdict;
     }
     mapping(address => Player) public players;
@@ -20,19 +21,41 @@ contract RandomNumbers {
     }
 
     event amountdeposit(uint256 deposit, uint256 playerid);
+    event AmountWithdraw(uint256 _amount, address user);
+    event BetAmount(uint256 _amount,address user,uint256 playerId);
 
     function DepositAmount() public payable {
         require(msg.value > 0, "invalid amount");
         playerId++;
-        moneypool+=msg.value;
+        moneypool += msg.value;
         players[msg.sender].deposit += msg.value;
         players[msg.sender].playerid = playerId;
         emit amountdeposit(msg.value, playerId);
     }
-    function generateRandomNumber() private  view returns (uint) {
-        uint randomHash = uint(keccak256(abi.encodePacked(block.timestamp, block.prevrandao)));
 
-        uint randomNumber = (randomHash % 50) + 1;
+    function generateRandomNumber() private view returns (uint256) {
+        uint256 randomHash = uint256(
+            keccak256(abi.encodePacked(block.timestamp, block.prevrandao))
+        );
+
+        uint256 randomNumber = (randomHash % 50) + 1;
         return randomNumber;
     }
+
+    function Withdraw(uint256 _amount) public {
+        require(_amount <= players[msg.sender].deposit, "invalid amount");
+        players[msg.sender].deposit -= _amount;
+        payable(msg.sender).transfer(_amount);
+        emit AmountWithdraw(_amount, msg.sender);
+    }
+
+    function placebet(uint256 _amount) public {
+        require(_amount > 0, "invalid amount");
+        players[msg.sender].betamount = _amount;
+        players[msg.sender].deposit -= _amount;
+        emit BetAmount(_amount, msg.sender, players[msg.sender].playerid);
+    }
+
+
+    
 }
